8964002583dc0ce783c9bacda47b27c1315281ac
Javier Antoran <javir803@gmail.com>
2017-05-14 19:41:30 +0200

    chamged output not wnid for tree trversing

--- server/classifier.py
+++ server/classifier.py
@@ -5,11 +5,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import classifier_interface
 
 import argparse
 import os.path
 import sys
+import re
 import tarfile
 
 import numpy as np
@@ -21,6 +21,79 @@ FLAGS = None
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
 
 
+class NodeLookup(object):
+  """Converts integer node ID's to human readable labels."""
+
+  node_id_to_wnid = {}
+
+  def __init__(self,
+               label_lookup_path=None,
+               uid_lookup_path=None):
+    if not label_lookup_path:
+      label_lookup_path = os.path.join(
+          FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+    if not uid_lookup_path:
+      uid_lookup_path = os.path.join(
+          FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
+    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+
+  def load(self, label_lookup_path, uid_lookup_path):
+    """Loads a human readable English name for each softmax node.
+
+    Args:
+      label_lookup_path: string UID to integer node ID.
+      uid_lookup_path: string UID to human-readable string.
+
+    Returns:
+      dict from integer node ID to human-readable string.
+    """
+    if not tf.gfile.Exists(uid_lookup_path):
+      tf.logging.fatal('File does not exist %s', uid_lookup_path)
+    if not tf.gfile.Exists(label_lookup_path):
+      tf.logging.fatal('File does not exist %s', label_lookup_path)
+
+    # Loads mapping from string UID to human-readable string
+    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+    uid_to_human = {}
+    p = re.compile(r'[n\d]*[ \S,]*')
+    for line in proto_as_ascii_lines:
+      parsed_items = p.findall(line)
+      uid = parsed_items[0]
+      human_string = parsed_items[2]
+      uid_to_human[uid] = human_string
+
+    # Loads mapping from string UID to integer node ID.
+    node_id_to_uid = {}
+    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+    for line in proto_as_ascii:
+      if line.startswith('  target_class:'):
+        target_class = int(line.split(': ')[1])
+      if line.startswith('  target_class_string:'):
+        target_class_string = line.split(': ')[1]
+        node_id_to_uid[target_class] = target_class_string[1:-2]
+
+    # Loads the final mapping of integer node ID to human-readable string
+    node_id_to_name = {}
+    for key, val in node_id_to_uid.items():
+      if val not in uid_to_human:
+        tf.logging.fatal('Failed to locate: %s', val)
+      name = uid_to_human[val]
+      node_id_to_name[key] = name
+      self.node_id_to_wnid[key] = val
+
+    return node_id_to_name
+
+  def id_to_wnid(self, node_id):
+      if node_id not in self.node_id_to_wnid:
+          return ''
+      return self.node_id_to_wnid[node_id]
+
+  def id_to_string(self, node_id):
+    if node_id not in self.node_lookup:
+      return ''
+    return self.node_lookup[node_id]
+
 def create_graph():
   """Creates a graph from saved GraphDef file and returns a saver."""
   # Creates graph from saved graph_def.pb.
@@ -29,7 +102,6 @@ def create_graph():
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
-
 
 def run_inference_on_image(image):
   """Runs inference on an image.
@@ -56,14 +128,15 @@ def run_inference_on_image(image):
     predictions = np.squeeze(predictions)
 
     # Creates node ID --> English string lookup.
-    node_lookup = classifier_interface.NodeLookup(FLAGS)
+    node_lookup = NodeLookup()
 
     top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
     for node_id in top_k:
         #MAKE CHANGES HERE
         human_string = node_lookup.id_to_string(node_id)
         score = predictions[node_id]
-        print('node: %s, %s (score = %.5f)' % (node_id, human_string, score))
+        wnid = node_lookup.id_to_wnid(node_id)
+        print('node: %s, %s (score = %.5f)' % (wnid, human_string, score))
 
 def maybe_download_and_extract():
   """Download and extract model tar file."""
@@ -96,7 +169,6 @@ def classify(target_img):
     #maybe_download_and_extract()
     image = (FLAGS.image_file)
     run_inference_on_image(image)
-
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
