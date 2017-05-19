b4212c3d8a673a06a1be0f799096970a236bb0cd
Javier Antoran Cabiscol <javir803@gmail.com>
2017-05-19 19:29:16 +0200

    Mathcing class for pictogram decision

--- server/picto_matcher.py
+++ server/picto_matcher.py
@@ -1,4 +1,5 @@
 import web
+import numpy as np
 from web.contrib.template import render_jinja
 import glob
 
@@ -8,17 +9,44 @@ urls = (
   '/', 'Index'
 )
 
-db_file = './database/synset.sqlite3'
+db_file = './../synsets/database/synset.sqlite3'
 
-db = web.database(dbn='sqlite', db=db_file)
+dbi = web.database(dbn='sqlite', db=db_file)
+
 
 class PictoMatcher(object):
 
-    def __init__(self, ):
+    def __init__(self, db, picto_wNid, thresh):
         pass
 
-    def match(self, picto, result_id, result_weights):
-        pass
+    def match(self, result_wNid, result_weights):
+
+        N_results = len(result_wNid)
+        prob = 0
+
+        for n_id, id in result_wNid:
+
+            next_id = id
+            while pnid != self.pictowNid and pnid is not None:
+
+                matches = self.db.query('''
+                                    SELECT pnid FROM imagenet_synset WHERE wnid = $id''', vars={'id': next_id})
+                pnid = np.unique(matches)
+
+                if len(pnid) != 1:
+                    print 'Multiple parents or no parents error: selecting first match'
+                    pnid = pnid[0]
+                next_id = pnid
+
+            if pnid is not None:
+                prob += result_weights[n_id]
+
+        return prob >= self.thresh
+
+
+
+
+
 
 
 
