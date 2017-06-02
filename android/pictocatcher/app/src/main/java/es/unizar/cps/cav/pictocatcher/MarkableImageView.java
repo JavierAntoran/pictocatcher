96f057d3736e233c27ffb414f8f3c3793ff53666
Alberto Mur <565825@unizar.es>
2017-06-02 21:37:49 +0200

    Checked bitmap in gridview

--- android/pictocatcher/app/src/main/java/es/unizar/cps/cav/pictocatcher/MarkableImageView.java
+++ android/pictocatcher/app/src/main/java/es/unizar/cps/cav/pictocatcher/MarkableImageView.java
@@ -6,7 +6,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
 
 /**
@@ -42,12 +45,75 @@ public class MarkableImageView extends ImageView {
         super.onDraw(canvas);
         if(checked) {
             Bitmap check = BitmapFactory.decodeResource(
-                    getResources(), R.drawable.checked);
+                    getResources(), R.drawable.checked2);
+
+           int imgSize = getContext().getResources().getDimensionPixelSize(R.dimen.gridimage);
 
             Rect r1 = new Rect(0,0,check.getWidth(),check.getHeight());
-            Rect r2 = new Rect(canvas.getWidth()-200,0, canvas.getWidth(),200);
+            Rect r2 = new Rect(canvas.getHeight()/2, canvas.getWidth()/2,
+                    canvas.getHeight(),canvas.getWidth());
 
             canvas.drawBitmap(check, r1, r2, new Paint());
+
         }
+    }
+
+    boolean mAdjustViewBounds;
+
+    @Override
+    public void setAdjustViewBounds(boolean adjustViewBounds) {
+        mAdjustViewBounds = adjustViewBounds;
+        super.setAdjustViewBounds(adjustViewBounds);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable mDrawable = getDrawable();
+        if (mDrawable == null) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+
+        if (mAdjustViewBounds) {
+            int mDrawableWidth = mDrawable.getIntrinsicWidth();
+            int mDrawableHeight = mDrawable.getIntrinsicHeight();
+            int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+            int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+            int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+            int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+
+            if (heightMode == MeasureSpec.EXACTLY && widthMode != MeasureSpec.EXACTLY) {
+                // Fixed Height & Adjustable Width
+                int height = heightSize;
+                int width = height * mDrawableWidth / mDrawableHeight;
+                if (isInScrollingContainer())
+                    setMeasuredDimension(width, height);
+                else
+                    setMeasuredDimension(Math.min(width, widthSize), Math.min(height, heightSize));
+            } else if (widthMode == MeasureSpec.EXACTLY && heightMode != MeasureSpec.EXACTLY) {
+                // Fixed Width & Adjustable Height
+                int width = widthSize;
+                int height = width * mDrawableHeight / mDrawableWidth;
+                if (isInScrollingContainer())
+                    setMeasuredDimension(width, height);
+                else
+                    setMeasuredDimension(Math.min(width, widthSize), Math.min(height, heightSize));
+            } else {
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            }
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    private boolean isInScrollingContainer() {
+        ViewParent p = getParent();
+        while (p != null && p instanceof ViewGroup) {
+            if (((ViewGroup) p).shouldDelayChildPressedState()) {
+                return true;
+            }
+            p = p.getParent();
+        }
+        return false;
     }
 }
